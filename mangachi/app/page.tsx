3a887ad69289c618d2e764a265ca537d64729a9c
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-12-28 22:30:37 +0530

    feat: add more horizontal scrollable components

--- mangachi/app/page.tsx
+++ mangachi/app/page.tsx
@@ -1,12 +1,16 @@
 import { CarouselDemo } from "@/components/Carsoul";
 import { ScrollAreaHorizontalDemo } from "@/components/HorizontalScrool";
-import { MobileCarousel } from "@/components/component/mobile-carousel";
+
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col items-center mb-60">
         <CarouselDemo />
-        <ScrollAreaHorizontalDemo />
+
+        <ScrollAreaHorizontalDemo title="Staff Picks"/>
+        <ScrollAreaHorizontalDemo title="Recently Added"/>
+        <ScrollAreaHorizontalDemo title="Most Readed"/>
+        <ScrollAreaHorizontalDemo title="Latest "/>
       </div>
     </main>
   );
--- mangachi/components/HorizontalScrool.tsx
+++ mangachi/components/HorizontalScrool.tsx
@@ -4,30 +4,35 @@ import Image from "next/image";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Mangas } from "@/app/data";
 
-export function ScrollAreaHorizontalDemo() {
+export function ScrollAreaHorizontalDemo({ title }: { title: string }) {
   return (
-    <ScrollArea className="mx-6 w-full whitespace-nowrap rounded-md border">
-      <div className="flex w-max space-x-4 p-4">
-        {Mangas.map((artwork) => (
-          <figure key={artwork.mangaName} className="shrink-0">
-            <div className="overflow-hidden rounded-md">
-              <Image
-                src={artwork.magaCoverImage}
-                alt={` manga ${artwork.mangaName}`}
-                className="aspect-[3/4] h-fit w-fit object-cover md:w-60 shadow-lg"
-                width={300}
-                height={400}
-              />
-            </div>
-            <figcaption className="pt-2 text-xs text-muted-foreground">
-              <span className="font-semibold text-foreground">
-                {artwork.mangaName}
-              </span>
-            </figcaption>
-          </figure>
-        ))}
-      </div>
-      <ScrollBar orientation="horizontal" />
-    </ScrollArea>
+    <div className="w-full">
+      <h2 className="mx-10 my-4 text-3xl font-semibold text-primary">
+        {title}
+      </h2>
+      <ScrollArea className="px-6 w-full whitespace-nowrap ">
+        <div className="flex w-max space-x-4 p-4">
+          {Mangas.map((artwork) => (
+            <figure key={artwork.mangaName} className="shrink-0">
+              <div className="overflow-hidden rounded-md">
+                <Image
+                  src={artwork.magaCoverImage}
+                  alt={` manga ${artwork.mangaName}`}
+                  className="aspect-[3/4] h-fit w-fit object-cover md:w-60 shadow-lg"
+                  width={300}
+                  height={400}
+                />
+              </div>
+              <figcaption className="pt-2 text-xs text-muted-foreground">
+                <span className="font-semibold text-foreground">
+                  {artwork.mangaName}
+                </span>
+              </figcaption>
+            </figure>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </div>
   );
 }
