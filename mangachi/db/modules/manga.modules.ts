478409ccba6b2c974fa9005606a20f358bac9799
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-01-03 16:57:29 +0530

    feat: manga model added

--- mangachi/db/modules/manga.modules.ts
+++ mangachi/db/modules/manga.modules.ts
@@ -1,2 +1,89 @@
 import mongoose from "mongoose";
- const MangaSchema = new mongoose.Schema()
+const MangaSchema = new mongoose.Schema(
+  {
+    mangaName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    backgroundImage: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      required: true,
+    },
+    tags: [
+      {
+        tag: {
+          type: String,
+          enum: [
+            "Action",
+            "Adventure",
+            "Fantasy",
+            "Romance",
+            "Slice of Life",
+            "Comedy",
+            "Drama",
+            "Mystery",
+            "Horror",
+            "Supernatural",
+            "Science Fiction",
+            "School Life",
+            "Historical",
+            "Isekai",
+            "Shounen",
+            "Shoujo",
+            "Seinen",
+            "Josei",
+            "Psychological",
+            "Thriller",
+            "Sports",
+            "Martial Arts",
+            "Superpower",
+            "Space",
+            "Cyberpunk",
+            "Harem",
+            "Ecchi",
+            "Gender Bender",
+            "Mecha",
+            "Post-Apocalyptic",
+            "Music",
+            "Magic",
+            "Vampire",
+            "Demons",
+            "Monsters",
+            "Detective",
+            "Historical",
+            "Military",
+            "Game",
+            "Illuminati",
+          ],
+        },
+      },
+    ],
+    likes: [],
+    dislikes: [],
+    reviews: [],
+    description: {
+      type: String,
+      required: true,
+    },
+    chapters: [
+      {
+        chapterName: { type: String, required: true, unique: true },
+        chapterPublishedDate: { type: Date, required: true },
+        chapterImage: { type: String, required: true },
+        chapterNumber: { type: Number, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Manga = mongoose.model("Manga", MangaSchema);
