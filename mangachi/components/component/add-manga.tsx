2595c2dcadbcdb2d586650961bb510dc5ba0e4dc
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-01-06 15:45:39 +0530

    ref: fixed the hydration error

--- mangachi/components/component/add-manga.tsx
+++ mangachi/components/component/add-manga.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
+
 import { useRouter } from "next/navigation";
 export function AddManga({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
     <div className="min-h-screen bg-white py-12 px-4 sm:px-6 lg:px-8">
       <div className=" w-full">
-        <h2 className="text-3xl font-extrabold text-gray-900 ">Settings</h2>
+        {/* <h2 className="text-3xl font-extrabold text-gray-900 ">Settings</h2>
         <p className="mt-2 text-sm ">
           Manage your account settings and set preferences for your manga
           collection.
@@ -14,7 +15,7 @@ export function AddManga({ children }: { children: React.ReactNode }) {
         <hr
           className="bg-primary  w-full my-8 rounded-full"
           style={{ height: "2px" }}
-        />
+        /> */}
         <div className="mt-8 flex flex-col lg:flex-row ">
           <div className="flex flex-col w-full lg:w-64">
             <Button
@@ -29,13 +30,13 @@ export function AddManga({ children }: { children: React.ReactNode }) {
             >
               Chapters
             </Button>
-
             <Button
               className="mb-4 block bg-white py-2 px-4 text-left text-sm font-medium text-gray-700 hover:bg-gray-50"
               onClick={() => router.push("/manga/add/preview")}
             >
               Preview
             </Button>
+
             <Button
               className="mb-4 block bg-white py-2 px-4 text-left text-sm font-medium text-gray-700 hover:bg-gray-50"
               onClick={() => router.push("/manga/add/comments")}
@@ -43,10 +44,10 @@ export function AddManga({ children }: { children: React.ReactNode }) {
               Comments
             </Button>
             <Button
-              className="block bg-white py-2 px-4 text-left text-sm font-medium text-gray-700 hover:bg-gray-50"
-              onClick={() => router.push("/manga/add/settings")}
+              className=" block bg-white py-2 px-4 text-left text-sm font-medium text-gray-700 hover:bg-gray-50"
+              onClick={() => router.push("/manga/add/comments")}
             >
-              Settings
+            Manga Settings
             </Button>
           </div>
           <div className="flex-1 bg-white p-6">{children}</div>
