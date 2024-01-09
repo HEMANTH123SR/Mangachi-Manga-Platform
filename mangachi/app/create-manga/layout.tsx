eeefe3bf489abcb911323b8f876c4dac5145f207
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-01-09 11:27:07 +0530

    ref the code and delted the add-manga component & for creating manga the route has changed

---
+++ mangachi/app/create-manga/layout.tsx
@@ -0,0 +1,20 @@
+
+const Page = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen bg-white pt-6 pb-12 px-4 sm:px-6 lg:px-8">
+      <div className=" w-full">
+        <h2 className="text-3xl font-extrabold text-gray-900">Upload Manga</h2>
+        <p className="mt-2 text-sm text-gray-600">
+          Share your manga with the world! Upload and manage your manga
+          collection easily.
+        </p>
+        <hr className="bg-primary h-[2px] my-6" />
+        <div className="mt-12 flex flex-col lg:flex-row ">
+          \<div className="flex-1 bg-white px-6">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- mangachi/app/manga/add/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { AddManga } from "@/components/component/add-manga";
-const Page = ({ children }: { children: React.ReactNode }) => {
-  return <AddManga>{children}</AddManga>;
-};
-
-export default Page;
